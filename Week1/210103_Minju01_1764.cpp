e92280c9afcdb159f57bc1aa8d7ff1cf00932f59
MIN-JU-CHO <60171052+MIN-JU-CHO@users.noreply.github.com>
2021-01-04 14:42:17 +0900

    Add files via upload

--- Week1/210103_Minju01_1764.cpp
+++ Week1/210103_Minju01_1764.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <string.h>
 
 using namespace std;
 
 string binsearch(vector<string> list, string key, int left, int right);
-vector<string> bubble_sort(vector<string> list, int n);
+void qsort(vector<string>& list, int start, int final);
 
 int main(void) {
 
@@ -31,6 +32,8 @@ int main(void) {
 		//cout << stringBuffer << " ";
 	}
 
+	
+
 	vector<string> secondNames;	//vector of second Names
 	secondNames.clear();	//initializing
 	for (int i = 0; i < numOfSecondNames; i++) {
@@ -39,21 +42,22 @@ int main(void) {
 		//cout << stringBuffer << " ";
 	}
 
+	//sort by dictionary
 	
+	qsort(firstNames, 0, numOfFirstNames-1);	//put index
+	qsort(secondNames, 0, numOfSecondNames-1);	//put index
 
 
 	vector<string> answers;		//vector of keys from binsearch (firstNames[i] == secondNames[i])
 	answers.clear();	//initializing
 	for (int i = 0; i < numOfFirstNames; i++) {
 		string answer;
-		answer = binsearch(secondNames, firstNames[i], 1, numOfSecondNames);	//index를 자연수로 바꿔줌. index: 0 ~ (numOfSecondNames-1)
+		answer = binsearch(secondNames, firstNames[i], 0, numOfSecondNames-1);	// index: 0 ~ (numOfSecondNames-1)
 		if (answer != "") {				//if the same word exist
 			answers.push_back(answer);	//push to answer (not sort yet)
 		}
 	}
 	
-	//sort by dictionary
-	answers = bubble_sort(answers, answers.size());
 
 	//output
 	cout << answers.size() << endl;
@@ -96,23 +100,68 @@ string binsearch(vector<string> list, string key, int left, int right) {
 			//printf("left = %d\n", left);
 		}
 	}
+	
 	return "";
 }
+/*
+void merge(vector<string>& list, int start, int final, int medium) {	// start index, final index, medium index
+	vector<string> ret;		//temp와 같은 기능
+	int i = start, j = medium + 1, copy = 0;
 
-vector<string> bubble_sort(vector<string> list, int n) {
-	int i, j;
-	string temp;
-
-	for (i = n - 1; i>0; i--) {
-		// 0 ~ (i-1)까지 반복
-		for (j = 0; j<i; j++) {
-			// j번째와 j+1번째의 요소가 크기 순이 아니면 교환
-			if (list[j]>list[j + 1]) {
-				temp = list[j];
-				list[j] = list[j + 1];
-				list[j + 1] = temp;
-			}
+	//배열 한 번 반복
+	while (i <= medium && j <= final) {		//start ~ medium, medium ~ final
+		if (list[i] < list[j]) {		//올바른 순서일 때
+			ret.push_back(list[i++]);	//앞에 것을 ret에 넣음 & i++
+		}
+		else if (list[i] > list[j]) {	//순서 바뀌었을 때
+			ret.push_back(list[j++]);	//뒤에 것을 ret에 넣음 & j++
 		}
 	}
-	return list;	//변경 사항 main에서도 적용되어야 함
+	
+	//배열 한 번 반복 후 push 안된 남은 것 처리
+	while (i <= medium) ret.push_back(list[i++]);	//남은 것 ret에 넣음
+	while (j <= final) ret.push_back(list[j++]);	//남은 것 ret에 넣음
+
+	//ret -> 원본 복사
+	for (int k = start; k <= final; k++) {
+		list[k] = ret[copy++];		//k는 원래 배열 index, copy는 복사본 배열 index
+	}
+	//인수 list의 자료형이 참조형이므로 (&) 원본 데이터 조작 가능
+}
+
+void mergeSort(vector<string>& list, int start, int final) {
+	if (start < final) {	//index valid value 인지 확인
+		int medium = (start + final) / 2;	//setting medium index
+		
+		//list will be divided into 2
+		mergeSort(list, start, medium);		//recall
+		mergeSort(list, medium + 1, final);		//recall
+
+		//merge & sort
+		merge(list, start, final, medium);
+	}
+	
+}
+*/
+void qsort(vector<string>& list, int start, int final) {
+	string pivot = list[start];
+	int bs = start, be = final;
+	while (start < final) {		//until switch start and final
+		while (pivot <= list[final] && start < final) {
+			final--;
+		}
+		if (start > final) break;
+		while (pivot >= list[start] && start < final) {
+			start++;
+		}
+		if (start > final) break;
+		swap(list[start], list[final]);
+	}
+	swap(list[bs], list[start]);
+	if (bs < start) {
+		qsort(list, bs, start - 1);
+	}
+	if (be > final) {
+		qsort(list, start + 1, be);
+	}
 }
