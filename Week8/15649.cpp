4b158cb6aee4b90a8baba99696efa0a632c208f1
onpyeong <70002218+onpyeong@users.noreply.github.com>
2021-02-23 16:16:48 +0900

    강수현_15649

---
+++ Week8/15649.cpp
@@ -0,0 +1,37 @@
+//Silver 3 <N과 M(1)> - 백트랙킹 -
+#include <iostream>
+using namespace std;
+
+int N, M;
+int p[10] = { 0 };
+bool used[10] = { 0 };
+
+void permutation(int k) {
+	if (k == M) {
+		for (int i = 0; i < M; i++) {
+			cout << p[i] << ' ';
+			//printf("%d ", p[i]);
+		}
+		cout << '\n';
+		//printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (!used[i]) {
+			p[k] = i;
+			used[i] = 1;
+			permutation(k + 1);
+			used[i] = 0;
+		}
+	}
+}
+
+int main() {
+	//https://codecollector.tistory.com/381
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> M;
+
+	permutation(0);
+}
